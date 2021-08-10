fa67af207f1526f89f2c60147258defd0e8e41ca
cyrusfirheir <56257647+cyrusfirheir@users.noreply.github.com>
2021-08-10 22:33:14 +0530

    init

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,5 +1,1 @@
-import { greet } from './greeter/greeting-string';
-
-setup.greeting = greet("world " + random(200));
-
 console.log("Hello from TS!");
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
 "use strict";
 
 const path = require("path");
+const webpack = require('webpack');
+const { VueLoaderPlugin } = require("vue-loader");
 
 module.exports = (env) => {
 	const mode = env.production ? "production" : "development";
+
+	const plugins = [
+		new VueLoaderPlugin()
+	];
+	if (env.production) plugins.push(new webpack.DefinePlugin({
+		__VUE_OPTIONS_API__: false,
+		__VUE_PROD_DEVTOOLS__: false,
+	}));
 
 	const watch = !env.production;
 	
 	return {
 		mode,
+		plugins,
 		watch,
 		target: "web",
 		entry: "./src/scripts/index.ts",
@@ -24,7 +35,23 @@ module.exports = (env) => {
 				{
 					test: /\.ts$/,
 					exclude: /node_modules/,
-					use: "ts-loader"
+					loader: "ts-loader",
+					options: {
+						appendTsSuffixTo: [/\.vue$/]
+					}
+				},
+				{
+					test: /\.vue$/,
+					exclude: /node_modules/,
+					use: "vue-loader"
+				},
+				{
+					test: /\.scss$/,
+					use: [
+						'vue-style-loader',
+						'css-loader',
+						'sass-loader'
+					]
 				}
 			]
 		}
