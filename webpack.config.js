246fb4b5c283e19442d216a3e1fd337d68d68dee
cyrusfirheir <56257647+cyrusfirheir@users.noreply.github.com>
2021-08-10 22:28:33 +0530

    webpack config update + deps update

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,32 @@
-//@ts-check
+"use strict";
 
-'use strict';
+const path = require("path");
 
-const path = require('path');
+module.exports = (env) => {
+	const mode = env.production ? "production" : "development";
 
-/**@type {import('webpack').Configuration}*/
-module.exports = {
-	target: 'web',
-	entry: './src/scripts/index.ts',
-	output: {
-		path: path.resolve(__dirname, 'src/__compiled'),
-		filename: 'index.js'
-	},
-	resolve: {
-		extensions: ['.ts', '.js']
-	},
-	module: {
-		rules: [
-			{
-				test: /\.ts$/,
-				exclude: /node_modules/,
-				use: 'ts-loader'
-			}
-		]
-	}
+	const watch = !env.production;
+	
+	return {
+		mode,
+		watch,
+		target: "web",
+		entry: "./src/scripts/index.ts",
+		output: {
+			path: path.resolve(__dirname, "src/__compiled"),
+			filename: "index.js"
+		},
+		resolve: {
+			extensions: [".ts", ".js"]
+		},
+		module: {
+			rules: [
+				{
+					test: /\.ts$/,
+					exclude: /node_modules/,
+					use: "ts-loader"
+				}
+			]
+		}
+	};
 };
